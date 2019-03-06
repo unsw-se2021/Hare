e88176c3479f645ce43e4d3a6c3101da7e974211
Michael-Lloyd <michaelwoodgerlloyd@gmail.com>
2019-03-06 10:00:12 +0000

    Containers completed

--- Deliverable_1_Documentation/Hi-Fidelity-Prototype/Hare_Frontend/src/components/App.js
+++ Deliverable_1_Documentation/Hi-Fidelity-Prototype/Hare_Frontend/src/components/App.js
@@ -1,34 +1,81 @@
 // Main container for website application. 
 import React, { Component } from 'react'; 
-import { Grommet, Box } from 'grommet'; 
+import { Grommet, grommet, Box, Heading, Menu } from 'grommet';
+import * as Icons from 'grommet-icons';
 import Theme from './Theme.js'; 
 import View from './View.js'; 
 
 class Navbar extends Component { 
-
-};
+	render() { 
+		return(
+			<Box
+				tag='header' // Tag that appears in rendered HTML
+				background='neutral-3' // Colour of fill 
+				pad='small' // Internal padding | --> contents <-- |
+				elevation='xsmall' // Drop shadow "distance" above ground
+				justify='between' // Pushes items inside of box to both sides
+				direction='row' // Stretch sideways instead of upwards 
+				flex={false}
+			>
+			<Heading level={3} margin='none'>
+				<strong>App</strong>
+			</Heading>
+			<Menu
+				items={
+					[
+						{
+							label: 'Logout',
+							href: '#'
+						}
+					]
+				}
+				icon={
+					<Icons.Apps color='white'/>
+				}
+			/>
+		</Box>
+		);
+	}
+} 
 
 class Body extends Component { 
-
-}; 
+	render() { 
+		return(
+			<Box 
+				flex={true}
+				id='Body'
+				background='grey'
+			>
+				BODY CONTENT
+			</Box>	
+		);
+	}
+}  
 
 class Footer extends Component { 
-
+	render() { 
+		return(
+			<Box
+				tag='footer'
+				direction='row'
+				flex={false}
+			>
+				FOOTER CONTENT
+			</Box>
+		);
+	}
 }; 
 
 class App extends Component { 
 	render() { 
-		return( 
-			<Grommet theme={Theme} full={true}>
-				<Box fill={true}> 
+		return(
+			<Grommet theme={grommet} full={true}>
+				<Box fill={true} alignContent="between">
 					<Navbar />
-					1
 					<Body />
-					2
-					<Footer /> 
-					3
+					<Footer />
 				</Box>
-			</Grommet> 
+			</Grommet>
 		); 
 	}
 } 
