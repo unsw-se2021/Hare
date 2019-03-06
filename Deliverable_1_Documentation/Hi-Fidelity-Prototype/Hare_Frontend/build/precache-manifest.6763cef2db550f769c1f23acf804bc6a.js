c95c3d873d48223f0d8806b12e1258219a3165ff
Michael-Lloyd <michaelwoodgerlloyd@gmail.com>
2019-03-06 02:14:15 +0000

    Updated serverfiles

---
+++ Deliverable_1_Documentation/Hi-Fidelity-Prototype/Hare_Frontend/build/precache-manifest.6763cef2db550f769c1f23acf804bc6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c6d60d8a433029cda619",
+    "url": "/static/js/main.c6d60d8a.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "c6d60d8a433029cda619",
+    "url": "/static/css/main.c7076545.chunk.css"
+  },
+  {
+    "revision": "056aa3b75fa5294a1cd0b442f802760d",
+    "url": "/index.html"
+  }
+];
---
+++ Deliverable_1_Documentation/Hi-Fidelity-Prototype/Hare_Frontend/src/components/Screen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Screen.css';
+
+class Screen extends Component {
+  render() {
+    return (
+	
+	
+	
+	
+	
+	
+	
+	);
+  }
+}
+
+export default Screen; 
