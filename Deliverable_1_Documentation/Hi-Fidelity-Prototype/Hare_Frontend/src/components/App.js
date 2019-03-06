6d66ac15e3462bdd749b4c709f1b838dc9fc9079
Michael-Lloyd <michaelwoodgerlloyd@gmail.com>
2019-03-06 10:41:59 +0000

    Layout overlay complete

--- Deliverable_1_Documentation/Hi-Fidelity-Prototype/Hare_Frontend/src/components/App.js
+++ Deliverable_1_Documentation/Hi-Fidelity-Prototype/Hare_Frontend/src/components/App.js
@@ -10,29 +10,43 @@ class Navbar extends Component {
 		return(
 			<Box
 				tag='header' // Tag that appears in rendered HTML
-				background='neutral-3' // Colour of fill 
-				pad='small' // Internal padding | --> contents <-- |
-				elevation='xsmall' // Drop shadow "distance" above ground
-				justify='between' // Pushes items inside of box to both sides
-				direction='row' // Stretch sideways instead of upwards 
-				flex={false}
-			>
+			background='neutral-3' // Colour of fill 
+			align='center'
+			pad='small' // Internal padding | --> contents <-- |
+			elevation='xsmall' // Drop shadow "distance" above ground
+			justify='between' // Pushes items inside of box to both sides
+			direction='row' // Stretch sideways instead of upwards 
+			flex={false}>
+
 			<Heading level={3} margin='none'>
 				<strong>App</strong>
 			</Heading>
-			<Menu
-				items={
-					[
-						{
-							label: 'Logout',
-							href: '#'
-						}
-					]
-				}
-				icon={
-					<Icons.Apps color='white'/>
-				}
-			/>
+			<Box direction="row">
+				<Menu
+					id='help_button'
+					items={
+						[
+							{ label: '1', onClick: () => {console.log("HELP 1 REQUESTED")} },
+							{ label: '2' },
+							{ label: '3' }
+						]
+					}
+					icon={
+						<Icons.Help color='white'/>
+					}	 
+					alignSelf='end'
+				/>
+				<Menu
+					items={
+						[
+							{label: 'Log in', onClick: () => {console.log("LOG IN REQUESTED")} }
+						]
+					}
+					icon={
+						<Icons.Apps color='white'/>
+					}
+				/>
+			</Box>
 		</Box>
 		);
 	}
@@ -44,7 +58,7 @@ class Body extends Component {
 			<Box 
 				flex={true}
 				id='Body'
-				background='grey'
+				background='white-2'
 			>
 				BODY CONTENT
 			</Box>	
@@ -55,12 +69,16 @@ class Body extends Component {
 class Footer extends Component { 
 	render() { 
 		return(
-			<Box
+			<Box 
 				tag='footer'
 				direction='row'
 				flex={false}
-			>
-				FOOTER CONTENT
+				background='neutral-3'
+				pad='small'
+				justify='between'
+				alignContent='end'>
+				<code>FOOTER CONTENT</code>
+				<code>FOOTER</code>
 			</Box>
 		);
 	}
@@ -70,7 +88,10 @@ class App extends Component {
 	render() { 
 		return(
 			<Grommet theme={grommet} full={true}>
-				<Box fill={true} alignContent="between">
+				<Box 
+					background-image="url(http://images7.memedroid.com/images/UPLOADED252/585e47b2e5c2a.jpeg)" 
+					fill={true} 
+					alignContent="between">
 					<Navbar />
 					<Body />
 					<Footer />
