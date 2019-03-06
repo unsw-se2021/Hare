6b5cf4c349325ff57b50ed880e825117f36df7cc
Michael-Lloyd <michaelwoodgerlloyd@gmail.com>
2019-03-06 06:04:39 +0000

    App container updated

--- Deliverable_1_Documentation/Hi-Fidelity-Prototype/Hare_Frontend/src/components/App.js
+++ Deliverable_1_Documentation/Hi-Fidelity-Prototype/Hare_Frontend/src/components/App.js
@@ -1,18 +1,36 @@
 // Main container for website application. 
 import React, { Component } from 'react'; 
-import { Grommet } from 'grommet'; 
+import { Grommet, Box } from 'grommet'; 
 import Theme from './Theme.js'; 
-
-class App extends Component { 
-
-};
+import View from './View.js'; 
 
 class Navbar extends Component { 
 
 };
 
+class Body extends Component { 
+
+}; 
+
 class Footer extends Component { 
 
 }; 
 
-export default App;
+class App extends Component { 
+	render() { 
+		return( 
+			<Grommet theme={Theme} full={true}>
+				<Box fill={true}> 
+					<Navbar />
+					1
+					<Body />
+					2
+					<Footer /> 
+					3
+				</Box>
+			</Grommet> 
+		); 
+	}
+} 
+
+export default App; 
