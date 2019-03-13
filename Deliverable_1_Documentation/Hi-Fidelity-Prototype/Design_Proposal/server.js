3f06cd9016e8c82b5b1564902340fce1d4b0a88c
Michael-Lloyd <michaelwoodgerlloyd@gmail.com>
2019-03-13 10:07:34 +0000

    Gitignore configured to ignore build directories

--- Deliverable_1_Documentation/Hi-Fidelity-Prototype/Design_Proposal/server.js
+++
@@ -1,8 +0,0 @@
-let path = require('path') ; 
-
-const express = require('express');
-const app = express();
-const port = 8080;
-
-app.use(express.static('public'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
