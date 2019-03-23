a10a16811578db48ca606325e3cb36e13181d812
root <root@ip-172-31-9-65.ap-southeast-2.compute.internal>
2019-03-23 04:22:00 +0000

    added a compare page

---
+++ Website_Demo/Hare_Frontend/src/components/compare.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Grommet, grommet, Box, Paragraph, Text, Accordion, AccordionPanel, Heading } from 'grommet';
+
+import * as Icons from 'grommet-icons';
+import { Route, Link, Router, BrowserRouter } from 'react-router-dom';
+
+class compare extends Component {
+	render() {
+	         return(
+			 <div>
+			 </div>
+			 	);
+	}
+}
+
+export default compare;
+
