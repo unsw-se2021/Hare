75d6268f0413b7c96f5d833d8ed85cd8a912f73b
julieduan <z5158357@student.unsw.edu.au>
2019-03-25 14:01:27 +1100

    updated getter functions

--- Website_Demo/Hare_App/database/get.js
+++ Website_Demo/Hare_App/database/get.js
@@ -1,21 +1,203 @@
-// gets data idk 
-
-
-const check_username = (user_auth, username) => { 
-	return  new Promise((resolve, reject) => { 
-		let user_count = 0;
-		user_auth.find({ 'user.name': username }, (err, result) => {
-			user_count = result.length; 
-			if(user_count > 0) {
-				reject(String(user_count));
-			} else { 
-				resolve(true); 
-			}
-		});
-
-	}); 
+//check duplication of user's name
+const check_username_unique = (user_collection, username) => {
+  return new Promise((resolve, reject) => {
+    user_collection.find({ "user.name": username }, (err, result) => {
+      const user_count = result.length;
+      if (user_count > 0) {
+        reject("Duplicated username");
+      } else {
+        resolve(true);
+      }
+    });
+  });
 };
 
+//check duplication of user's email
+const check_useremail_unique = (user_collection, useremail) => {
+  return new Promise((resolve, reject) => {
+    user_collection.find({ "user.email": useremail }, (err, result) => {
+      const user_count = result.length;
+      if (user_count > 0) {
+        reject("Duplicated user email");
+      } else {
+        resolve(true);
+      }
+    });
+  });
+};
+
+//check password and username for login
+const check_login_withUsername = (user_collection, username, userpws) => {
+  return new Promise((resolve, reject) => {
+    user_collection.find(
+      { "user.name": username, "user.password": userpws },
+      (err, result) => {
+        const user_count = result.length;
+        if (user_count === 0) {
+          reject("Username / password invalid");
+        } else {
+          resolve(true);
+        }
+      }
+    );
+  });
+};
+
+//check password and useremail for login
+const check_login_withUseremail = (user_collection, useremail, userpws) => {
+  return new Promise((resolve, reject) => {
+    user_collection.find(
+      { "user.email": useremail, "user.password": userpws },
+      (err, result) => {
+        const user_count = result.length;
+        if (user_count === 0) {
+          reject("Useremail / password invalid");
+        } else {
+          resolve(true);
+        }
+      }
+    );
+  });
+};
+
+//get userID by username
+const get_userID_by_username = (user_collection, username) => {
+  return new Promise((resolve, reject) => {
+    user_collection.find({ "user.name": username }, (err, result) => {
+      const user_count = result.length;
+      if (user_count === 0) {
+        reject("invalid username");
+      } else {
+        const user_id = result[0].id;
+        resolve(user_id);
+      }
+    });
+  });
+};
+
+//get userId by useremail
+const get_userID_by_useremail = (user_collection, useremail) => {
+  return new Promise((resolve, reject) => {
+    user_collection.find({ "user.email": useremail }, (err, result) => {
+      const user_count = result.length;
+      if (user_count === 0) {
+        reject("invalid useremail");
+      } else {
+        const user_id = result[0].id;
+        resolve(user_id);
+      }
+    });
+  });
+};
+
+//get username by userId
+const get_username_by_userId = (user_collection, username) => {
+  return new Promise((resolve, reject) => {
+    user_collection.find({ "user.name": username }, (err, result) => {
+      const user_count = result.length;
+      if (user_count === 0) {
+        reject("invalid useremail");
+      } else {
+        const user_id = result[0].id;
+        resolve(user_id);
+      }
+    });
+  });
+};
+
+//get useremail by userId
+const get_useremail_by_userId = (user_collection, useremail) => {
+  return new Promise((resolve, reject) => {
+    user_collection.find({ "user.email": useremail }, (err, result) => {
+      const user_count = result.length;
+      if (user_count === 0) {
+        reject("invalid useremail");
+      } else {
+        const user_id = result[0].id;
+        resolve(user_id);
+      }
+    });
+  });
+};
+
+//write the similiar functions for profile,product and rest of the object
+
+//get preference by userId
+const get_preference_by_userId = (profile_collection, userId) => {
+  return new Promise((resolve, reject) => {
+    profile_collection.find({ "profile.uid": userId }, (err, result) => {
+      const user_count = result.length;
+      if (user_count === 0) {
+        reject("invalid userId");
+      } else {
+        const preference_id = result.preferences;
+        const preference = profile_collection.children.id(preference_id);
+        resolve(preference);
+      }
+    });
+  });
+};
+
+//get categories by preference
+const get_categories_by_preferences = (preferences_collection, preferences) => {
+  return new Promise((resolve, reject) => {
+    preferences_collection.find(
+      { "preferences.db_id": preferences },
+      (err, result) => {
+        const preferences_count = result.length;
+        if (preferences_count === 0) {
+          reject("invalid preferences");
+        } else {
+          const catergories = result.catergories;
+          resolve(catergories);
+        }
+      }
+    );
+  });
+};
+
+//get categories by userId
+const get_categories_by_userId = (profile_collection, userId) => {
+  return new Promise((resolve, reject) => {
+    profile_collection.find({ "peofile.uid": userId }, (err, result) => {
+      const user_count = result.length;
+      if (user_count === 0) {
+        reject("invalid userId");
+      } else {
+        const preference_id = result.preferences;
+        const preference = profile_collection.children.id(preference_id);
+        const catergories = preference.catergories;
+        resolve(catergories);
+      }
+    });
+  });
+};
+
+//what left is :
+//get email status
+//get preference by userId
+//get ingredients by preference
+//get ingredients by userId
+
+//location is production collection according to ducumentation librabry
+//get product by userId
+//get ingredients by product
+//get images by product
+
+//get name from ingrients collection
+//get synonyms from ingrients collection
+//get description from ingrients collection
+//get drugbank link from ingrients collection
+//get tally from ingrients collection
+
 module.exports = {
-	check_username: check_username
-}
+  check_username_unique: check_username_unique,
+  check_useremail_unique: check_useremail_unique,
+  check_login_withUsername: check_login_withUsername,
+  check_login_withUseremail: check_login_withUseremail,
+  get_username_by_userId: get_username_by_userId,
+  get_useremail_by_userId: get_useremail_by_userId,
+  get_preference_by_userId: get_preference_by_userId,
+  get_categories_by_preferences: get_categories_by_preferences,
+  get_categories_by_userId: get_categories_by_userId
+};
