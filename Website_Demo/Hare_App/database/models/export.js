534739ff12793b086aca61293122e1a8795facb2
julieduan <z5158357@student.unsw.edu.au>
2019-03-27 14:17:46 +1100

    added create ingredients functions and getters for history and changed model

--- Website_Demo/Hare_App/database/models/export.js
+++ Website_Demo/Hare_App/database/models/export.js
@@ -1,12 +1,12 @@
 const db_model = {
-	ingredient: require('./user_auth'),
-	preferences: require('./preferences').preferences,
-	category_preference: require('./preferences').category_preference, 
-	ingredient_preference: require('./preferences').ingredient_preference, 
-	product: require('./product'),
-	profile: require('./profile'),
-	user_auth: require('./user_auth'),
-	history: require('./history'), 
-}
+  ingredient: require("./ingredient"),
+  preferences: require("./preferences").preferences,
+  category_preference: require("./preferences").category_preference,
+  ingredient_preference: require("./preferences").ingredient_preference,
+  product: require("./product"),
+  profile: require("./profile"),
+  user_auth: require("./user_auth"),
+  history: require("./history")
+};
 
-module.exports = db_model; 
+module.exports = db_model;
