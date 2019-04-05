8a56f10a7f92e2df57b16232f75274e82e1ec30a
julieduan <z5158357@student.unsw.edu.au>
2019-04-05 14:07:57 +1100

    added db_ids to some models

--- Website_Demo/Hare_App/database/models/ingredient.js
+++ Website_Demo/Hare_App/database/models/ingredient.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const IngredientSchema = new Schema({ 
-	name: { type: String, required: true }, 
-	synonyms: [String], 
-	description: String, 
-	wikilink: String,
-	tally: Number
-}); 
+const IngredientSchema = new Schema({
+  db_id: { type: Schema.Types.ObjectId, required: true },
+  name: { type: String, required: true },
+  synonyms: [String],
+  description: String,
+  wikilink: String,
+  tally: Number
+});
 
-module.exports = ingredient = mongoose.model('ingredient', IngredientSchema); 
+module.exports = ingredient = mongoose.model("ingredient", IngredientSchema);
