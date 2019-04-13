ac08f9fdfd83cec51707b7d6b4386f8941cfaa8e
Ravija Maheshwari <ravija@Ravijas-MacBook-Pro.local>
2019-04-13 17:29:05 +1000

    Uniqueness checking added to schema

--- Website_Demo/Hare_App/database/models/user_auth.js
+++ Website_Demo/Hare_App/database/models/user_auth.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const validator = require('validator');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const UserAuthSchema = new Schema({
 	uid: { 
@@ -7,12 +9,28 @@ const UserAuthSchema = new Schema({
 		required: true, 
 		description: "Must be a string, and is the root of user profile schema linkages"
 	},
-	user: { 
-		name: String, 
-		email: String,
-		password: String
+	user: {
+		name: {
+			type: String,
+			unique: true,
+			required: [true, "can't be empty"]
+		},
+		email: {
+			type: String,
+			lowercase: true,
+			validate: (value)=>{
+				return validator.isEmail(value)
+			}
+		},
+		password: {
+			type: String,
+			required: [true, "can't be empty"]
+		},
 	}, 
 	timestamp: Date 
 });
+
+//Ensuring uniqueness
+UserAuthSchema.plugin(uniqueValidator, {message: 'Is already taken.'});
 
 module.exports = user_auth = mongoose.model('user_auth', UserAuthSchema); 
